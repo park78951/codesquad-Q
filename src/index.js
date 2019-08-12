5a8b671e5fcfe2a5c9ef237d556ee2edc11f54db
Sangkyu <park78951@gmail.com>
2019-08-12 16:21:43 +0900

    For deploy

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import App from './components/App';
 
 import { hot } from 'react-hot-loader/root';
 
-const Hot = process.env.NODE_ENV === 'development'
-  ? hot(App)
-  : App;
+const Hot = process.env.NODE_ENV === 'production'
+  ? App
+  : hot(App);
 
 ReactDOM.render(<Hot />, document.getElementById('root'));
